5bcf26ba5ba665c4856d0c9a068623e2aa436cb5
sranso <sranso@gmail.com>
2014-02-25 19:57:23 -0500

    heroku?

--- app.rb
+++ app.rb
@@ -8,7 +8,19 @@ end
 
 class Inspiration < Sinatra::Application
   configure :development do
-    set :database, "sqlite3:///database.db"
+    set :database, 'sqlite3:///database.db'
+  end
+
+  configure :production do
+    db = URI.parse( || 'postgres:///localhost/')
+    ActiveRecord::Base.establish_connection(
+      :adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+      :host => db.host,
+      :username => db.user,
+      :password => db.password,
+      :database => db.path[1..-1],
+      :encoding => 'utf8'
+    )
   end
 
   # show everything
