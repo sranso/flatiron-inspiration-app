29ef2e24d33da06a99e36ffc70b4b8c993647b81
sranso <sranso@gmail.com>
2014-03-04 14:56:20 -0500

    edit buttons and forms and labels

--- public/app/js/animation.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-  $(".new-quote-link").on("click", function(event) {
-    // event.preventDefault();
-    console.log("this??");
-    // $(this)
-    $(".new-quote-form").toggle("slow");
-  });
-});
